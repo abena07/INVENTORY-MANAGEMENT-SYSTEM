defe7ef6d692ff6140beb2943561b0cd92136177
abena07 <abenabennett@gmail.com>
2022-08-28 21:40:25 +0000

    Create User Screen Modified

--- Inventory_Mgt_Sys/CreateUser.cs
+++ Inventory_Mgt_Sys/CreateUser.cs
@@ -23,6 +23,9 @@ namespace Inventory_Mgt_Sys
         private ComboBox role;
         private Label label7;
         private ComboBox gender;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
         private TextBox firstName;
 
         public CreateUser()
@@ -46,11 +49,17 @@ namespace Inventory_Mgt_Sys
             this.role = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
             this.gender = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // firstName
             // 
-            this.firstName.Location = new System.Drawing.Point(84, 49);
+            this.firstName.Location = new System.Drawing.Point(23, 79);
             this.firstName.MaximumSize = new System.Drawing.Size(180, 40);
             this.firstName.MinimumSize = new System.Drawing.Size(180, 40);
             this.firstName.Name = "firstName";
@@ -60,7 +69,7 @@ namespace Inventory_Mgt_Sys
             // 
             // lastName
             // 
-            this.lastName.Location = new System.Drawing.Point(84, 131);
+            this.lastName.Location = new System.Drawing.Point(373, 79);
             this.lastName.MaximumSize = new System.Drawing.Size(180, 40);
             this.lastName.MinimumSize = new System.Drawing.Size(180, 40);
             this.lastName.Name = "lastName";
@@ -71,7 +80,8 @@ namespace Inventory_Mgt_Sys
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 21);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(23, 51);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 25);
             this.label1.TabIndex = 3;
@@ -81,7 +91,8 @@ namespace Inventory_Mgt_Sys
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(84, 103);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(364, 51);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 25);
             this.label2.TabIndex = 4;
@@ -91,7 +102,8 @@ namespace Inventory_Mgt_Sys
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(82, 188);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(368, 38);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(115, 25);
             this.label3.TabIndex = 5;
@@ -102,7 +114,7 @@ namespace Inventory_Mgt_Sys
             // 
             this.dob.CustomFormat = "dd/MM/yyyy";
             this.dob.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dob.Location = new System.Drawing.Point(82, 216);
+            this.dob.Location = new System.Drawing.Point(368, 66);
             this.dob.MaximumSize = new System.Drawing.Size(180, 40);
             this.dob.MinimumSize = new System.Drawing.Size(180, 40);
             this.dob.Name = "dob";
@@ -112,7 +124,7 @@ namespace Inventory_Mgt_Sys
             // 
             // userName
             // 
-            this.userName.Location = new System.Drawing.Point(82, 303);
+            this.userName.Location = new System.Drawing.Point(27, 68);
             this.userName.MaximumSize = new System.Drawing.Size(180, 40);
             this.userName.MinimumSize = new System.Drawing.Size(180, 40);
             this.userName.Name = "userName";
@@ -122,7 +134,7 @@ namespace Inventory_Mgt_Sys
             // 
             // password
             // 
-            this.password.Location = new System.Drawing.Point(82, 397);
+            this.password.Location = new System.Drawing.Point(377, 69);
             this.password.MaximumSize = new System.Drawing.Size(180, 40);
             this.password.MinimumSize = new System.Drawing.Size(180, 40);
             this.password.Name = "password";
@@ -133,7 +145,8 @@ namespace Inventory_Mgt_Sys
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(80, 369);
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(377, 41);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(87, 25);
             this.label4.TabIndex = 9;
@@ -143,7 +156,8 @@ namespace Inventory_Mgt_Sys
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(82, 275);
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(27, 41);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(85, 25);
             this.label5.TabIndex = 10;
@@ -154,7 +168,7 @@ namespace Inventory_Mgt_Sys
             // 
             this.Add.BackColor = System.Drawing.Color.SlateBlue;
             this.Add.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Add.Location = new System.Drawing.Point(152, 613);
+            this.Add.Location = new System.Drawing.Point(532, 199);
             this.Add.Name = "Add";
             this.Add.Size = new System.Drawing.Size(112, 42);
             this.Add.TabIndex = 11;
@@ -165,7 +179,8 @@ namespace Inventory_Mgt_Sys
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(84, 535);
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(27, 51);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(46, 25);
             this.label6.TabIndex = 12;
@@ -178,7 +193,7 @@ namespace Inventory_Mgt_Sys
             this.role.Items.AddRange(new object[] {
             "Admin",
             "Attendant"});
-            this.role.Location = new System.Drawing.Point(82, 563);
+            this.role.Location = new System.Drawing.Point(27, 74);
             this.role.Name = "role";
             this.role.Size = new System.Drawing.Size(182, 33);
             this.role.TabIndex = 13;
@@ -188,7 +203,8 @@ namespace Inventory_Mgt_Sys
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(80, 455);
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(43, 136);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(69, 25);
             this.label7.TabIndex = 15;
@@ -201,35 +217,71 @@ namespace Inventory_Mgt_Sys
             this.gender.Items.AddRange(new object[] {
             "Male",
             "Female"});
-            this.gender.Location = new System.Drawing.Point(84, 483);
+            this.gender.Location = new System.Drawing.Point(29, 164);
             this.gender.Name = "gender";
             this.gender.Size = new System.Drawing.Size(182, 33);
             this.gender.TabIndex = 16;
             this.gender.SelectedIndexChanged += new System.EventHandler(this.gender_SelectedIndexChanged);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.firstName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lastName);
+            this.groupBox1.Location = new System.Drawing.Point(84, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(677, 150);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.userName);
+            this.groupBox2.Controls.Add(this.password);
+            this.groupBox2.Location = new System.Drawing.Point(80, 201);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(677, 150);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.groupBox3.Controls.Add(this.gender);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.role);
+            this.groupBox3.Controls.Add(this.Add);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.dob);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(80, 382);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(677, 256);
+            this.groupBox3.TabIndex = 19;
+            this.groupBox3.TabStop = false;
+            // 
             // CreateUser
             // 
             this.BackColor = System.Drawing.Color.Lavender;
-            this.Controls.Add(this.gender);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.role);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.Add);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.password);
-            this.Controls.Add(this.userName);
-            this.Controls.Add(this.dob);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.lastName);
-            this.Controls.Add(this.firstName);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "CreateUser";
-            this.Size = new System.Drawing.Size(534, 673);
+            this.Size = new System.Drawing.Size(806, 673);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -329,6 +381,11 @@ namespace Inventory_Mgt_Sys
         }
 
         private void gender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
         {
 
         }
